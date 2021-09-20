877261780d389c67e1ee7e028c7bdb81e1680e2b
Thomas VEILLARD <thomasveillard@live.fr>
2021-09-20 11:34:19 +0200

    Ex 8: color palette and prop for BaseButton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,19 +1,44 @@
 <template>
-  <button class="button">
+  <button 
+    class="button"
+    :style="getCssVariables"
+  >
     <slot />
   </button>
 </template>
 
 <script>
+const colorPalette = {
+  primary: { bg: '#42b983', hoverBg: '#4cce93', focusBorder: '#47d696' },
+  warn: { bg: '#ff5722', hoverBg: '#ff7043', focusBorder: '#ff8a65' },
+  danger: { bg: '#e53935', hoverBg: '#ef5350', focusBorder: '#e57373' },
+}
+
 export default {
-  name: 'BaseButton'
+  name: 'BaseButton',
+  props: {
+    color: {
+      type: /** @type {import('vue').PropType<keyof colorPalette>} */ (String),
+      default: 'primary',
+      validator: (/** @type {string} */ color) => color in colorPalette
+    }
+  },
+  computed: {
+    getCssVariables () {
+      return {
+        '--button-background-color': colorPalette[this.color].bg,
+        '--button-hover-background-color':  colorPalette[this.color].hoverBg,
+        '--button-focus-boder': colorPalette[this.color].focusBorder
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
   .button {
     padding: 0.75em;
-    background-color: #42b983;
+    background-color: var(--button-background-color);
     border: none;
     color: white;
     border-radius: 0.25em;
@@ -22,12 +47,12 @@ export default {
   }
 
   .button:hover {
-    background-color: #4cce93;
+    background-color: var(--button-hover-background-color);
   }
 
   .button:focus {
     padding: calc(0.75em - 2px);
-    border: 2px solid #47d696;
+    border: 2px solid var(--button-focus-boder);
   }
 
   .button:disabled {
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,18 @@
 <template>
   <div style="text-align: center">
     <p>Here comes the content of the HomePage.</p>
+
+    <p><base-button>BaseButton</base-button></p>
+    <p><base-button disabled>BaseButton disabled</base-button></p>
+    <p><base-button color="warn">BaseButton warn color</base-button></p>
+    <p><base-button color="danger">BaseButton danger color</base-button></p>
   </div>
 </template>
 
 <script>
+import BaseButton from './BaseButton.vue'
 export default {
+  components: { BaseButton },
   name: 'HomePage'
 }
 </script>
